9de13b27472c12f25ba603b74097fadaa435824d
Martin Hořeňovský <martin.horenovsky@gmail.com>
2015-05-20 17:59:25 +0200

    Normalized memcpyassert.c example.

--- examples/memcpyassert.c
+++ examples/memcpyassert.c
@@ -2,11 +2,16 @@
 #include <string.h>
 #include <assert.h>
 
+int read_int(){
+    int res;
+    klee_make_symbolic(&res, sizeof(res), "summand");
+    return res;
+}
 
 int main(){
         int a, b;
+        a = read_int();
 
-        klee_make_symbolic(&a, sizeof(a), "a");
         memcpy(&b, &a, sizeof(b));
 
         assert(b != 0);
